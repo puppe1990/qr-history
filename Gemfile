52d8dd06b30804232f0b9ce88fb3a0a072acfe0d
puppe1990 <matheus.puppe90@hotmail.com>
2019-12-14 16:04:48 -0300

    Create qr code to history

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'actiontext'
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
 gem 'rqrcode'
+gem 'rqrcode_png'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,3 +1,9 @@
 class History < ApplicationRecord
   has_rich_text :content
+  require 'rqrcode'
+  require 'rqrcode_png'
+
+  def generate_qrcode
+    qrcode = RQRCode::QRCode.new("https://conte-sua-historia.herokuapp.com/histories/#{id}")
+  end
 end
