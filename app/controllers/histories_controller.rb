c5b4e66f885fbe7f120b290f959ba30dc26e54cd
puppe1990 <matheus.puppe90@hotmail.com>
2019-12-20 10:24:53 -0300

    skip verification show history

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,6 @@
 class HistoriesController < ApplicationController
   before_action :set_history, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, :only => [:show]
 
   # GET /histories
   # GET /histories.json
