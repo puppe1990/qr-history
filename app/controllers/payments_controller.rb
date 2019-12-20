e6acacbe28d70033521a1b150bf9473b8d3c493a
puppe1990 <matheus.puppe90@hotmail.com>
2019-12-19 21:23:34 -0300

    Add new field

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,13 +13,14 @@ class PaymentsController < ApplicationController
   def create_qr_payment
     price = 0.01
     total = price * params[:quantity].to_i
-    @sale = Sale.create(quantity: params[:quantity].to_i, price: price, total: total, user_id: current_user.id, status: 'pending')
+    reference_id = rand(100..1000000)
+    @sale = Sale.create(reference_id: reference_id, quantity: params[:quantity].to_i, price: price, total: total, user_id: current_user.id, status: 'pending')
     uri = URI.parse("https://appws.picpay.com/ecommerce/public/payments")
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/json"
     request["X-Picpay-Token"] = ENV["X-Picpay-Token"]
     request.body = JSON.dump({
-      "referenceId" => (11 + @sale.id).to_s,
+      "referenceId" => reference_id.to_s,
       "callbackUrl" => "https://conte-sua-historia.herokuapp.com/payments/callback",
       "returnUrl" => "https://conte-sua-historia.herokuapp.com",
       "value" => total,
@@ -48,18 +49,20 @@ class PaymentsController < ApplicationController
 
   def callback
     uri = URI.parse("https://appws.picpay.com/ecommerce/public/payments/#{params["referenceId"]}/status")
+    @sale = Sale.where(reference_id: params["referenceId"]).first
     request = Net::HTTP::Get.new(uri)
     request.content_type = "application/json"
     request["X-Picpay-Token"] = ENV["X-Picpay-Token"]
-    
+
     req_options = {
       use_ssl: uri.scheme == "https",
     }
-    
+
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-    @sale = Sale.update(status: response['status'])
+    response = JSON.parse(response.body)
+    @sale.update(status: response['status'])
   end
 
   private
